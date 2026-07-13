03e917b2443f4cddfe57732abe8632eb1a62afb1
Vivi <70679402+vivivivianne@users.noreply.github.com>
2026-07-13 12:35:10 -0300

    Update credentials.h

--- bracelet/src/credentials.h
+++ bracelet/src/credentials.h
@@ -1,6 +1,6 @@
 // CREDENTIAL CONFIGS, update your configs here
-#define WIFI_SSID "Vi"
-#define WIFI_PASS "35220410"
-#define APP_KEY "c5c02bc5-8434-40ba-b097-2b4904dbeb57"
-#define APP_SECRET "f497c718-d614-4172-a405-76f23c064744-fd886b53-9ff1-4a5f-8249-6a1b84a3a7c3"
-#define BRACELET_ID "6a528acf6ba33a80b98dfb57"
+#define WIFI_SSID "wifi_SSID"
+#define WIFI_PASS "PASSWD"
+#define APP_KEY "c5c02bc5j-485434-40ba-b097-2b4904dbeb57"
+#define APP_SECRET "f4958718-d614-4172-a405-76f2764064744-fd886b53-9ff1-4a5f-8249-6a1b84a3a7c3"
+#define BRACELET_ID "6a52rhcf6ba33ashb98dfb57"
